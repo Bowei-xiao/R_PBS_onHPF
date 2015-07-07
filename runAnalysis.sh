6640cf5b27705185a90e092b627a8d556d489530
Bowei-xiao <work.adam.xiao@gmail.com>
2015-07-07 10:22:44 -0400

    Supporting file for the main code
    This is the bash code that was actually run in the main script
    The Analysis was done in R (see the R codes)

---
+++ runAnalysis.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+# Syntax: runAnalysis.sh data1 data2 phenotype ChrNumber
+
+f1=$1   # data from GW1
+f2=$2   # data from GW2
+i=$3    # Chromosome number
+# Tor siblings were added to JHU cohort so that 
+# 1. we could run logistic using Toronto samples, reducing calculation time
+# 2. More importantly, Toronto sample may not converge since there are so few clusters
+# To sum up, now only run JHU using GEE but run the other 3 using logistics
+# Actual code was in R
+
+
+Rscript 0601Meta_log.R ${f1} ${f2} 'fam_fr.fam' ${i}
+Rscript 0601Meta_log.R ${f1} ${f2} 'fam_tor.fam' ${i}
+Rscript 0601Meta_log.R ${f1} ${f2} 'fam_unc.fam' ${i}
+Rscript 0601Meta_gee.R ${f1} ${f2} 'fam_jhu.fam' ${i}
+echo 'Analysis is Done' 
